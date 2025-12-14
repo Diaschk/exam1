7e9e32b6203f1356c023d6c0eb2e72c630d9f4d0
Diaschk <yovhymyschdiana@gmail.com>
2025-12-14 23:42:02 +0200

    Створення програми

---
+++ test_app.py
@@ -0,0 +1,19 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import main
+import pytest
+
+def test_sum_progression():
+
+    assert sum([2]) == 2
+    
+    assert sum([2, 4, 6]) == 12
+    
+    assert sum([2, 4, 6, 8, 10]) == 30
+    
+    print("Всі тести пройдені")
+
+if __name__ == "__main__":
+    test_sum_progression()
